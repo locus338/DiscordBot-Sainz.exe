7b9fb63807871c22922c123fbb7da7d1fe81aa97
locus338 <114900398+locus338@users.noreply.github.com>
2022-10-04 09:11:11 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -75,58 +75,25 @@ async def on_raw_reaction_remove(payload):
 
 
 # 收到訊息時呼叫
-@client.event
-async def on_message(message):
-    
+@client.command()
+async def translate(ctx, *, message):
     # 送信者為Bot時無視
-    if message.author.bot:
-        return
-    await client.process_commands(message)
-    #私訊
-    if message.guild == None:
-        return
-    result = service.spreadsheets().values().get(
-    spreadsheetId=SpreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
-    if not values:
+    if ctx.message.author == client.user:
+        print("逼逼逼")
         return
     else:
-        for row in values:
-            if (message.channel.name == row[0] or row[0] == ''):
-                keywords = row[1].split()
-                check = True
-                for keyword in keywords:
-                    if not keyword in message.content:
-                        check = False
-                        break
-                if check:
-                    if message.author.nick == None:
-                        username = message.author.name
-                    else:
-                            username = message.author.nick
-                    if '<ban>' in row[2]:
-                        await message.author.ban()
-                    else:
-                        if '<kick>' in row[2]:
-                            await message.author.kick()
-                        if '<delete>' in row[2]:
-                            await message.delete()
-                        else:
-                            if '<reply>' in row[2]:
-                                await message.reply(row[3].replace('<username>',username))
-                            if '<replyrandom>' in row[2]:
-                                msgs = row[3].split('|')
-                                if len(msgs) != 0:
-                                    index = random.randint(0, len(msgs)-1)
-                                    await message.reply(msgs[index].replace('<username>',username))
-                    if '<send>' in row[2]:
-                        await message.channel.send(row[3].replace('<username>',username))
-                    if '<sendrandom>' in row[2]:
-                        msgs = row[3].split('|')
-                        if len(msgs) != 0:
-                            index = random.randint(0, len(msgs)-1)
-                            await message.channel.send(msgs[index].replace('<username>',username))
-                    return
-    
+        print("執行成功!")
+        translator = googletrans.Translator()
+        if translator.detect(message).lang == SRCLanguage or SRCLanguage == '' or SRCLanguage == 'zh-CN': #判斷text是其他語言則翻成中文
+            pass
+        else:
+            print(message)
+            print(translator.detect(message).lang) 
+            if translator.detect(message).lang != "zh-TW":
+                remessage = translator.translate(message, dest='zh-TW').text #翻成中文
+                await ctx.reply(remessage)
+            if translator.detect(message).lang != "en":
+                remessageen = translator.translate(message, dest='en').text #翻成英文
+                await ctx.reply(remessageen)
 # Bot起動
 client.run(TOKEN)
