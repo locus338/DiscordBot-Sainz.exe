b26f92d369ab62e6c0ece24383796369850c3815
locus338 <114900398+locus338@users.noreply.github.com>
2022-10-27 06:43:04 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,3 +1,4 @@
+import googletrans
 from distutils.log import error
 import discord
 from discord.ext import commands
@@ -7,7 +8,12 @@ import os
 from apiclient import discovery
 import random
 import asyncio
-token = os.getenv['TOKEN']
+token = os.getenv(TOKEN)
+
+
+
+
+SRCLanguage= "zh-TW"
 
 
 
@@ -27,25 +33,24 @@ async def on_ready():
 
 
 # 收到訊息時呼叫
-@client.command(aliases=['tran''t'])
+@client.command(aliases=['trans''t'])
 async def translate(ctx, *, message):
     # 送信者為Bot時無視
     if ctx.message.author == client.user:
         print("逼逼逼")
         return
     else:
-      print("執行成功!")
-      translator = googletrans.Translator()
-      if translator.detect(message).lang == SRCLanguage or SRCLanguage == '' or SRCLanguage == 'zh-CN': #判斷text是其他語言則翻成中文
-         pass
-       else:
-         print(message)
-         print(translator.detect(message).lang) 
-         if translator.detect(message).lang != "zh-TW":
-            remessage = translator.translate(message, dest='zh-TW').text #翻成中文
-            await ctx.reply(remessage)
-         if translator.detect(message).lang != "en":
-            remessageen = translator.translate(message, dest='en').text #翻成英文
-            await ctx.reply(remessageen)
+        print("執行成功!")
+        translator = googletrans.Translator()
+       if translator.detect(message).lang == 'zh-CN': #判斷text是其他語言則翻成中文
+    pass
+else:
+    print(translator.detect(message).lang)
+    if translator.detect(message).lang != "zh-TW":
+        remessage = translator.translate(message, dest='zh-TW').text #翻成中文
+        await ctx.reply(remessage)
+    if translator.detect(message).lang != "en":
+        remessageen = translator.translate(message, dest='en').text #翻成英文
+        await ctx.reply(remessageen)
 # Bot起動
-client.run(TOKEN)
+client.run(TOKEN) 
