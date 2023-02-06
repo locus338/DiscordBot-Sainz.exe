8a0326474d1b3e6b415870fcfb9f1917baecbdab
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-06 23:21:41 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -54,7 +54,7 @@ async def translate(ctx, *, message: typing.Optional[str] = None):
 @bot.event
 async def on_raw_reaction_add(payload): 
   #判斷反映貼圖給予相對應身分組
-  if user_payload.message_id == 1072171521193300021:
+  if payload.message_id == 1072171521193300021:
      if str(payload.emoji) == '✅':
         print("有進來")
         guild = bot.get_guild(payload.guild_id) # 取得當前所在伺服器
