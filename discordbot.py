0641bdd470ffbffb51a2dbdce3fc14b4bb117eb7
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-07 01:48:52 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,6 +60,15 @@ async def on_raw_reaction_add(payload):
         guild = bot.get_guild(payload.guild_id) # 取得當前所在伺服器
         role = guild.get_role(1072098531910881290) #取得伺服器內指定的身分組
         await payload.member.add_roles(role) # 給予該成員身分組
+@bot.event
+async def on_raw_reaction_remove(payload):
+  if payload.message_id == 1072171521193300021:
+     if str(payload.emoji) == '✅':
+        # 取得伺服器
+        guild = bot.get_guild(payload.guild_id)
+        user = await guild.fetch_member(payload.user_id)
+        await user.remove_roles(guild.get_role(1072098531910881290))
+        await user.send(f"已移除{role}身分組"")
 
 if __name__ == "__main__":
     token = os.getenv("TOKEN")    
