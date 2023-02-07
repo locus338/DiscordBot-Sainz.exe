6d962e105bf3b5cba957aa9d9b38ef6bba917e33
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-07 23:22:25 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -69,7 +69,7 @@ async def on_raw_reaction_remove(payload):
         user = await guild.fetch_member(payload.user_id)
         await user.remove_roles(guild.get_role(1072098531910881290))
 
-@bot.command()
+@bot.command(aliases=['PING',' 'pings', 'Ping','Pings']
 async def ping(ctx):
    await ctx.send(F'{round(bot.latency*1000)} (ms)')  
 
