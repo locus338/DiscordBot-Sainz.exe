19a747fb4dfa25817ae3c7e5b53b76cbe46057be
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-09 00:15:28 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -77,9 +77,6 @@ async def on_ready():
 @bot.command(aliases=['PING', 'PINGS', 'pings', 'Ping', 'Pings'])
 async def ping(ctx):
    await ctx.send(F'{round(bot.latency*1000)} (ms)')  
-@bot.command()
-async def help(ctx):
-   await ctx.send(f"敬請期待")
 
 if __name__ == "__main__":
     token = os.getenv("TOKEN")    
