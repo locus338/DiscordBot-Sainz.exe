f42bd5b375efe9b3a98e9cc6a0cbfe9147ab06b1
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-10 00:18:27 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -74,10 +74,6 @@ async def on_ready():
     game = discord.Game('EK的電腦')
     #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
     await bot.change_presence(status=discord.Status.online, activity=game)
-@bot.event
-async def on_message(message):
-  if message.content == 'https://disocrd.gg/':
-     await message.delete()
 @bot.command(aliases=['PING', 'PINGS', 'pings', 'Ping', 'Pings'])
 async def ping(ctx):
    await ctx.send(F'{round(bot.latency*1000)} (ms)')  
