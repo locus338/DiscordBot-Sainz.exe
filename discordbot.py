562acfd4a31aec41735d3e6ec7a5520ac4db7a1b
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-09 17:28:16 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -76,7 +76,7 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=game)
 @bot.event
 async def on_message(message):
-    if message.content == '我好帥喔':
+    if message.content == 'discord.gg':
         await message.delete()
 @bot.command(aliases=['PING', 'PINGS', 'pings', 'Ping', 'Pings'])
 async def ping(ctx):
