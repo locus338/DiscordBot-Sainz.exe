8ae7bc3f76d0174f1c6dc5151bfb2d1a40c59553
locus338 <114900398+locus338@users.noreply.github.com>
2023-02-15 21:50:20 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,14 +1,14 @@
 import discord
 from discord.ext import commands
-from core.classes import Cog_Extension
 
-class Main(Cog_Extension):
+
+class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.command(aliases=['PING', 'PINGS', 'pings', 'Ping', 'Pings'])
-    async def ping(self,ctx):
-        await ctx.send(F'{round(bot.latency*1000)}ms')
-        
-def setup(bot):
-    bot.add_cog(Main(bot))
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(self.bot.latency)
+
+async def setup(bot):
+  await bot.add_cog(Main(bot))
