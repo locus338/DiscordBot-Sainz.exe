818edb9e19869c0987329e8de96a42819183a394
locus338 <114900398+locus338@users.noreply.github.com>
2023-04-30 00:16:32 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,7 +8,7 @@ from flask import Flask, render_template
 intent = discord.Intents.all()
 intent.message_content = True
 app = Flask(__name__,template_folder="Templates")
-bot = commands.Bot(command_prefix="~", intents=intent)
+bot = commands.Bot(command_prefix="~", intents=intent , help_command=None)
 
 def run():
     app.run(host='0.0.0.0', port=10000, use_reloader=False, debug=True)
