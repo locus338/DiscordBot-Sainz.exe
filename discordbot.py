5da49a52dd87cfe392565aa775958acfe1dd1478
locus338 <114900398+locus338@users.noreply.github.com>
2023-05-02 19:05:58 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -69,6 +69,10 @@ async def on_raw_reaction_remove(payload):
         user = await guild.fetch_member(payload.user_id)
         await user.remove_roles(guild.get_role(1101890697822474330))
 @bot.event
+async def on_message(message):
+    if message.content == '我好帥喔':
+        await message.delete()
+@bot.event
 async def on_ready():
     print('目前登入身份：',bot.user)
     game = discord.Game('EK的電腦')
