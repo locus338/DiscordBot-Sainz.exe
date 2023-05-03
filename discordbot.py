8a22b60c9d31be9f08a49ac78962bd9fefa12de1
locus338 <114900398+locus338@users.noreply.github.com>
2023-05-03 21:21:00 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -50,24 +50,6 @@ async def translate(ctx, *, message: typing.Optional[str] = None):
        if translator.detect(message).lang != "en":
            remessageen = translator.translate(message, dest='en').text  # 翻成英文
            await ctx.reply(remessageen)
-
-@bot.event
-async def on_raw_reaction_add(payload): 
-  #判斷反映貼圖給予相對應身分組
-  if payload.message_id == 1101811571891650580:
-     if str(payload.emoji) == '✅':
-        print("有進來")
-        guild = bot.get_guild(payload.guild_id) # 取得當前所在伺服器
-        role = guild.get_role(1101890697822474330) #取得伺服器內指定的身分組
-        await payload.member.add_roles(role) # 給予該成員身分組
-@bot.event
-async def on_raw_reaction_remove(payload):
-  if payload.message_id == 1101811571891650580:
-     if str(payload.emoji) == '✅':
-        # 取得伺服器
-        guild = bot.get_guild(payload.guild_id)
-        user = await guild.fetch_member(payload.user_id)
-        await user.remove_roles(guild.get_role(1101890697822474330))
 @bot.event
 async def on_ready():
     print('目前登入身份：',bot.user)
