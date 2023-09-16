6ae4cb816147be77bd0841aee7b42c7b815bcb20
locus338 <114900398+locus338@users.noreply.github.com>
2023-09-16 21:54:25 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -60,7 +60,7 @@ async def ping(ctx):
 
 @bot.command(help_command=None)
 async def help(ctx):
-    await ctx.send(f"~`translate`開始翻譯                                                                                                                                                                        別名：`trans` `t` `T` `Trans` `TRANS`")
+    await ctx.send(f"~`translate`開始翻譯                                                                                                                                                                                                  別名：`trans` `t` `T` `Trans` `TRANS`")
 
 if __name__ == "__main__":
     token = os.getenv("TOKEN")    
